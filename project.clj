e9cd06cd9a825163b5398e67d85c878df37b4410
Richard Nagelmaeker <richard@theinfotect.org>
2017-08-08 09:06:16 +0200

    update dependencies in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject prefix-component "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+(defproject org.volenta/datamos.prefix "0.1.0-SNAPSHOT"
+  :description "Prefix module for dataMos."
+  :url "http://theinfotect.org/datamos"
+  :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE, Version 3"
+            :url "https://www.gnu.org/licenses/agpl-3.0.nl.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.volenta/datamos "0.1.3"]])
