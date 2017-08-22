c67840fcc997acc07936d03f3ae87fa9eef0aac7
Richard Nagelmaeker <richard@theinfotect.org>
2017-08-22 17:39:11 +0200

    bump prefix moduel and dataMos to version v0.1.6.2

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.volenta/datamos.prefix "0.1.6.1"
+(defproject org.volenta/datamos.prefix "0.1.6.2"
   :description "Prefix module for dataMos."
   :url "http://theinfotect.org/datamos"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE, Version 3"
             :url "https://www.gnu.org/licenses/agpl-3.0.nl.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [org.volenta/datamos "0.1.6.1"]
+                 [org.volenta/datamos "0.1.6.2"]
                  [com.taoensso/timbre "4.10.0"]
                  [mount "0.1.11"]]
   :main ^:skip-aot datamos.prefix.core
